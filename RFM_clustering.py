847839134e3fe92d46c4ca5d7e5968fef9cec1b2
ksehat <k.sehat@ut.ac.ir>
2022-11-25 11:52:17 +0330

    initial commit.

---
+++ RFM_clustering.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import plotly.express as px
+from sklearn.preprocessing import Normalizer, MinMaxScaler, MaxAbsScaler, StandardScaler, QuantileTransformer
+
+df = pd.read_excel('G:\Python projects\Ofogh_Kurosh\data\Filtered data/RFM values and segments.xlsx')
