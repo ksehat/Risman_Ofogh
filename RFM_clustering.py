e57c4a46f684de01d09e4f3020168f555f6feba9
ksehat <k.sehat@ut.ac.ir>
2022-11-27 11:40:57 +0330

    clustering with DBSCAN.

--- RFM_clustering.py
+++ RFM_clustering.py
@@ -1,8 +1,18 @@
 import pandas as pd
-import numpy as np
-import math
-import matplotlib.pyplot as plt
-import plotly.express as px
 from sklearn.preprocessing import Normalizer, MinMaxScaler, MaxAbsScaler, StandardScaler, QuantileTransformer
+from sklearn.cluster import KMeans, DBSCAN
+from yellowbrick.cluster import KElbowVisualizer
 
 df = pd.read_excel('G:\Python projects\Ofogh_Kurosh\data\Filtered data/RFM values and segments.xlsx')
+df1 = df.filter(['recency','frequency','monetary'])
+
+tr1 = DBSCAN()
+df_tr1 = tr1.fit_transform(df1)
+
+kmeans = KMeans(n_clusters=7, random_state=0).fit(df_tr1)
+# visualizer = KElbowVisualizer(kmeans, k=(4,12))
+# visualizer.fit(df_tr1)        # Fit the data to the visualizer
+# visualizer.show()        # Finalize and render the figure
+df['Cluster'] = kmeans.labels_
+
+df.to_excel('G:\Python projects\Ofogh_Kurosh\data\Filtered data/RFM values and Clusters by KMeans.xlsx')
